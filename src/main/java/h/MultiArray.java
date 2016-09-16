69aba021d13af6dd3919ff530b95a39bb255c924
Your Name <you@example.com>
2016-09-16 20:59:52 +0300

    four tipes of TwoFive

--- src/main/java/h/MultiArray.java
+++ src/main/java/h/MultiArray.java
@@ -14,23 +14,23 @@ public class MultiArray {
             }
         }
 
-        graph[0][0] = 0;
-        graph[1][0] = 0;
-        graph[0][1] = 0;
-
-//        graph[2][4] = 0;
-//        graph[2][5] = 0;
-//        graph[3][4] = 0;
+        graph[0][0] = '+';
+        graph[1][0] = '+';
+        graph[0][1] = '+';
 
 
-//        for (int v = 0; v < SIZE; v++) {
+//        graph[0][3] = '+';
+//        graph[2][4] = '+';
+//        graph[2][5] = '+';
+//        graph[3][4] = '+';
+
+
         int vert = 0;
         for (int h = 0; h < SIZE; h++) {
             if (graph[vert][h] == '#') {
                 tryTwoFive(vert, h);
             }
         }
-//        }
 
         for (int v = 0; v < SIZE; v++) {
             for (int h = 0; h < SIZE; h++) {
@@ -41,28 +41,35 @@ public class MultiArray {
     }
 
     public void tryTwoFive(int vert, int hor) {
-        if ((vert < SIZE - 1) && (graph[vert + 1][hor] == '#')) {
+        if ((vert < (SIZE - 1)) && (graph[vert + 1][hor] == '#')) {
             if ((hor < (SIZE - 1)) && (graph[vert + 1][hor + 1] == '#')) {
                 if ((hor < (SIZE - 2)) && (graph[vert + 1][hor + 2] == '#')) {
-                    graph[vert][hor] = 0;
-                    graph[vert + 1][hor] = 0;
-                    graph[vert + 1][hor + 1] = 0;
-                    graph[vert + 1][hor + 2] = 0;
+                    graph[vert][hor] = '+';
+                    graph[vert + 1][hor] = '+';
+                    graph[vert + 1][hor + 1] = '+';
+                    graph[vert + 1][hor + 2] = '+';
                 }
             } else if ((vert < (SIZE - 2) && (graph[vert + 2][hor] == '#'))) {
                 if ((hor > 0) && (graph[vert + 2][hor - 1] == '#')) {
-                    graph[vert][hor] = 0;
-                    graph[vert + 1][hor] = 0;
-                    graph[vert + 2][hor] = 0;
-                    graph[vert + 2][hor - 1] = 0;
+                    graph[vert][hor] = '+';
+                    graph[vert + 1][hor] = '+';
+                    graph[vert + 2][hor] = '+';
+                    graph[vert + 2][hor - 1] = '+';
                 }
                 if ((hor < (SIZE - 1)) && (graph[vert][hor + 1] == '#')) {
-                    graph[vert][hor] = 0;
-                    graph[vert + 1][hor] = 0;
-                    graph[vert + 2][hor] = 0;
-                    graph[vert][hor + 1] = 0;
+                    graph[vert][hor] = '+';
+                    graph[vert + 1][hor] = '+';
+                    graph[vert + 2][hor] = '+';
+                    graph[vert][hor + 1] = '+';
                 }
             }
+        } else if ((hor < SIZE + 2) && (vert < SIZE + 1) && (graph[vert][hor + 1] == '#')
+                && (graph[vert][hor + 2] == '#')
+                && (graph[vert + 1][hor + 2] == '#')) {
+            graph[vert][hor] = '+';
+            graph[vert][hor + 1] = '+';
+            graph[vert][hor + 2] = '+';
+            graph[vert + 1][hor + 2] = '+';
         }
     }
 
